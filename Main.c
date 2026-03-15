70295266ddb32a714e7f3a35fb454b3c37ebfc5d
Toma Sasaki <netst915@gmail.com>
2026-03-15 23:33:40 +0000

    a

--- Main.c
+++ Main.c
@@ -1,8 +1,31 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+typedef struct
+{
+    int8_t x;
+    int8_t y;
+} Point;
+
+typedef struct
+{
+    Point movingBallLocation;
+    int8_t paddleLocation;
+    Point remainingBalls[16];
+} GameState;
+
 int main(void)
 {
+    GameState currentGameState;
+    currentGameState.movingBallLocation.x = 6;
+    currentGameState.movingBallLocation.y = 0;
+    currentGameState.paddleLocation = 0;
+    for (int i = 0; i < 16; i++)
+    {
+        currentGameState.remainingBalls[i].x = i % 8;
+        currentGameState.remainingBalls[i].y = (int8_t)i / 8;
+    }
+
     for (;;)
     {
     }
